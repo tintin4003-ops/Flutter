8b4d806db7ffe21482725ddb1e68e1a2ad551ccd
tintin4003-ops <tintin4003@gmail.com>
2026-08-17 22:05:07 +0600

    update

--- class_01.dart
+++ class_01.dart
@@ -20,7 +20,40 @@ void main()
 
   //final vs const
   final currentTime= DateTime.now();//final can update on runtime
-  const curT= DateTime.now();// cant update works like constant .const update on compile time
+  //const curT= DateTime.now();// cant update works like constant .const update on compile time
   const pi = 3.1416;
-  
+  //null safety cant take  null directly as input
+  //String name3= null;
+  String? imgUrl = null;
+  print (imgUrl ?? "default.png");
+
+
+  //operators
+int a=10;
+int b=3;
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+  print(a%b);
+
+ //assignment op 
+
+ int p =5;
+ p+=5;
+ p-=2;
+ p*=2;
+
+ //inc,dec
+
+ p++;
+ p--;
+
+ //comparison operators
+ int age=20;
+ print(age==20);
+ print(age>18);
+ print(age<19);
+ print(age>=22);
+ print(age!=20);
 }
